787920c879c2e5ba256e2dddafa4219b036c89e8
y44k0v <17897299+y44k0v@users.noreply.github.com>
2026-03-26 23:06:21 -0400

    Add weekend homework update

--- 17-mar26/fractions.py
+++ 17-mar26/fractions.py
@@ -1,7 +1,11 @@
-# use all operators to overload
+# overload all operators 
+# Arithmetic: + (__add__), - (__sub__), * (__mul__), / (__truediv__)
+# Comparison: == (__eq__), != (__ne__), < (__lt__), > (__gt__)
 
 class Fraction:
     
     def __init__(self, num, den):
         self.num = num
-        self.den = den
+        self.den = den 
+        
+        
